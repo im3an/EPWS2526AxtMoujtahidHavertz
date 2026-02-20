625e66f6473e2972b01c824c3ea52564bbfb79df
Louis Axt <louis.axt@smail.th-koeln.de>
2026-02-20 19:28:19 +0100

    added Infopanel pet properties in map

--- Implementation/findmypet/src/main/kotlin/de/thk/gm/ep/findmypet/controller/missingreportConrtoller.kt
+++ Implementation/findmypet/src/main/kotlin/de/thk/gm/ep/findmypet/controller/missingreportConrtoller.kt
@@ -1,14 +1,21 @@
 package de.thk.gm.ep.findmypet.controller
 
+import de.thk.gm.ep.findmypet.services.MissingReportService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import java.util.*
 
 @Controller()
 @RequestMapping("/")
-class missingreportConrtoller {
-    @GetMapping("map")
-    fun map(): String {
+class missingreportConrtoller(
+    private val missingReportService: MissingReportService
+) {
+    @GetMapping("map/{missingReportId}")
+    fun map(model: Model, @PathVariable missingReportId: UUID): String {
+        model.addAttribute("missingReport",missingReportService.getById(missingReportId))
         return "map"
     }
 
